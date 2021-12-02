7d66227ce9a8dd9210caea56498e0c9e5491b017
Eunseo-KANG <92796869+Eunseo-KANG@users.noreply.github.com>
2021-12-02 21:25:31 +0900

    Initial commit

---
+++ models/Coin.js
@@ -0,0 +1,13 @@
+//coin.js
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const coinSchema = new Schema({
+  name: { type: String, unique: true },
+  coingeckoId: String,
+  isActive: Boolean,
+});
+
+const Coin = mongoose.model('Coin', coinSchema);
+
+module.exports = Coin;
---
+++ models/Key.js
@@ -0,0 +1,12 @@
+// Key.js
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const keySchema = new Schema({
+  publicKey: String,
+  secretKey: String,
+});
+
+const Key = mongoose.model('Key', keySchema);
+
+module.exports = Key;
